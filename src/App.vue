d8164fd09a97afd1f343e8e18b0719852c76d052
ammduncan <ammielgyanyawson@gmail.com>
2021-11-16 14:31:47 +0000

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      Rock Paper Scissors
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "@fontsource/barlow-semi-condensed/300.css"
+import "@fontsource/barlow-semi-condensed/400.css"
+import "@fontsource/barlow-semi-condensed/500.css"
+import "@fontsource/barlow-semi-condensed/600.css"
+import "@fontsource/barlow-semi-condensed/700.css"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    name: 'App',
+    components: {
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Barlow Semi Condensed", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*text-align: center;*/
+  color: white;
+  background-image: radial-gradient(circle at 50% 0%, hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  height: 100vh;
+  /*margin-top: 60px;*/
+}
+
+.container {
+  width: 90%;
+  max-width: 700px;
+  margin: auto;
 }
 </style>
